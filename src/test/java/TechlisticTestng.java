fbadec4cb8cec0af2cd3f3276c0e6220e866131a
SubodhShukla <subodh.shukla95@gmail.com>
2024-12-04 19:21:13 +0530

    updated add testng.xml file

--- src/test/java/TechlisticTestng.java
+++ src/test/java/TechlisticTestng.java
@@ -1,12 +1,15 @@
 import TechlisticClassPackage.TechlisticBaseClass;
+import org.openqa.selenium.devtools.v127.browser.Browser;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TechlisticTestng {
 
    @Test
-    public void enterDetails(){
+   @Parameters("Browsername")
+    public void enterDetails(String Browser){
         TechlisticBaseClass tbc=new TechlisticBaseClass();
-        tbc.BrowserConfig("Chrome");
+        tbc.BrowserConfig(Browser);
         System.out.println("Browser launched");
         System.out.println("pushed on dev enviornment ");
 
