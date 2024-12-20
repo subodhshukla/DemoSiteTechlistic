203bea328b136b074ad4100c89d454e1e4321f1e
SubodhShukla <subodh.shukla95@gmail.com>
2024-12-20 16:28:07 +0530

    resume driver close method

--- src/test/java/TechlisticTestng.java
+++ src/test/java/TechlisticTestng.java
@@ -26,27 +26,27 @@ public class TechlisticTestng extends TechlisticBaseClass{
         Thread.sleep(2000);
 
     }
-   /* @Test (priority = 2)
+    @Test (priority = 2)
     public void validateHomePage(){
         String homepageExpectedTitle="CHEMSCOPE LABSOLUTION INDIA LLP";
         String homepageActualTile= hpage.verifyHomePage();
-       *//* try {*//*
+        try {
             Assert.assertEquals(homepageActualTile, homepageExpectedTitle, "home page text not matching");
             System.out.println("Assertion Passed: Page title matches.");
-       *//* }*//*
-       *//* catch (AssertionError e) {
+        }
+        catch (AssertionError e) {
             System.err.println("Assertion Failed: " + e.getMessage());
             throw e; // Fail the test
-        }*//*
-    }*/
+        }
+    }
    /* @Test (priority = 2)
     public void loginToTestSite() throws InterruptedException {
        userlogin.login();
     }
 */
-  /*  @Test (priority = 3)
+    @Test (priority = 3)
     public void quitNow(){
        closeBrowser();
-    }*/
+    }
    //driver.quit();
 }
